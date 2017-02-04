a101f5e29c999c4be005053298a8b4bb905a9fa8
Eashaan Kumar <eashaan@kumar.co>
2017-02-03 21:28:22 -0500

    Lesson 1

---
+++ SpaceyGame/Assets/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float xInput = Input.GetAxis ("Horizontal");
+		float yInput = Input.GetAxis ("Vertical");
+
+		Vector2 mousePos = Input.mousePosition;
+		Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint (mousePos);
+
+		Vector3 input = new Vector3 (xInput, yInput, 0);
+		transform.position += input * 2 * Time.deltaTime;
+	
+		Debug.Log (worldMousePosition);
+		//transform.LookAt (worldMousePosition);
+
+		// get direction you want to point at
+		Vector2 direction = (worldMousePosition - (Vector2) transform.position).normalized;
+
+		// set vector of transform directly
+		transform.up = direction;
+
+	}
+}
