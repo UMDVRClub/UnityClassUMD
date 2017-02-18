fc2911a1bcf84d13c9f8a0947184cc430475c7e6
Eashaan Kumar <eashaan@kumar.co>
2017-02-17 19:43:00 -0500

    Lesson3 Collision Detection

---
+++ Lesson3-Feb10-SpaceyGame/Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public Transform player;
+
+	public Gun[] guns;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// get direction you want to point at
+		Vector3 direction = (player.position - transform.position).normalized;
+
+		// set vector of transform directly
+		transform.up = direction;
+
+		// fire
+		if (Input.GetMouseButton (0)) {
+			//create bullet
+			foreach (Gun gun in guns) {
+				gun.Fire ();
+			}
+		}
+	}
+}
---
+++ Lesson3-Feb10-SpaceyGame/Assets/HealthManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+	public float START_HEALTH;
+
+	private float currentHealth;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = START_HEALTH;
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentHealth <= 0) {
+			Die ();
+		}
+		//Debug.Log (gameObject.name + " has " + currentHealth + " health.");
+	}
+
+	void Die(){
+		//TODO: Kill yo self
+
+	}
+
+	public void TakeDamage(float dmg){
+		if (dmg <= 0)
+			return;
+		currentHealth -= dmg;
+		spriteRenderer.color = new Color (currentHealth, currentHealth, currentHealth);
+	}
+		
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.gameObject.name.Contains("Laser")) {
+			TakeDamage (0.2f);
+			Debug.Log ("I'm hit");
+		}
+	}
+}
